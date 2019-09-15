2311817aa6d8e3571e26352f0bea4907d08c7aec
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-09-15 23:18:18 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -3,8 +3,36 @@ class Triangle
   attr_accessor :a, :b, :c
 
   def initialize(a, b, c)
-    @a = a
-    @b = b
-    @c = c
+    if condition
+      
+    else
+      @a = a
+      @b = b
+      @c = c
+    end
+  end
+
+  def valid?(a, b, c)
+    if a <= 0 || b <= 0 || c <= 0
+      false
+    elsif a + b < c || a + c < b || b + c < a
+      false
+    else
+      true
+    end
+  end
+  
+  def kind
+    if @a == @b && @a == @c
+      :equilateral
+    elsif @a == @b && @a != @c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+    
   end
 end
